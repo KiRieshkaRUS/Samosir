518824a343dd331d7e0e801ec4639359e8e95b9c
Kostritsa Kirill <kir300604@yandex.ru>
2024-10-22 16:31:07 +0300

    done paragraph 8

--- Samosir_project/Samosir_project/main/models.py
+++ Samosir_project/Samosir_project/main/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
+
 class Projects(models.Model):
     project_name = models.CharField(max_length=250, verbose_name='Название проекта',
                                     blank=True, null=True)
     description = models.TextField(verbose_name='Описание', blank=True, null=True)
-    data_added = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
+    date_added = models.DateTimeField(auto_now=True, verbose_name='Дата добавления')
     price = models.FloatField(verbose_name='Цена', blank=True, null=True)
 
     def __str__(self):
@@ -13,4 +14,3 @@ class Projects(models.Model):
     class Meta:
         verbose_name = 'Проект'
         verbose_name_plural = 'Проекты'
-
--- Samosir_project/Samosir_project/main/views.py
+++ Samosir_project/Samosir_project/main/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .models import *
 
 
 def index(request):
     return render(request, 'main/index.html')
+
+
+def my_projects(request):
+    projects = Projects.objects.order_by('-date_added')
+
+    context = {'projects': projects}
+
+    return render(request, 'main/my_projects.html', context)
