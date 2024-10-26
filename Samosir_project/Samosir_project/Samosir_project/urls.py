f4da29f052209da0ad5a14682d3bcf59045d3552
Kostritsa Kirill <kir300604@yandex.ru>
2024-10-26 18:50:17 +0300

    done 9 paragraph

--- Samosir_project/Samosir_project/Samosir_project/urls.py
+++ Samosir_project/Samosir_project/Samosir_project/urls.py
@@ -22,6 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
+    path('users/', include('users.urls')),
 ]
 
 if settings.DEBUG:
