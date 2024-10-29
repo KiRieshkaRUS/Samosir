9ca163742081f7156bcbc4c167a6db7bcaea3172
Kostritsa Kirill <kir300604@yandex.ru>
2024-10-30 01:03:34 +0300

    Привязана БД (но не сделана)

--- Samosir_project/Samosir_project/users/models.py
+++ Samosir_project/Samosir_project/users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    username = models.CharField(max_length=300, verbose_name='Имя пользователя')
+    first_name = models.CharField(max_length=300, verbose_name='Имя')
+    last_name = models.CharField(max_length=300, verbose_name='Фамилия')
+    email = models.EmailField(verbose_name='Email')
+    avatar = models.ImageField(upload_to='media/user_avatars', verbose_name='Аватар',
+                               blank=True, null=True)
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        verbose_name = 'Профиль пользователя'
+        verbose_name_plural = 'Профили пользователей'
