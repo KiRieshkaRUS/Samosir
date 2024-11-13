4014fe4bfe579e63c0c0ee4db719f92f3e86d086
Kostritsa Kirill <kir300604@yandex.ru>
2024-11-13 03:09:15 +0300

    Осталось сделать запросы

--- NEW_Samosir_project/Samosir_project/Samosir_project/settings.py
+++ NEW_Samosir_project/Samosir_project/Samosir_project/settings.py
@@ -79,8 +79,11 @@ WSGI_APPLICATION = 'Samosir_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'NEW_Samosi',
+        'USER': 'postgres',
+        'PASSWORD': '12345678',
+        'HOST': 'localhost'
     }
 }
 
--- NEW_Samosir_project/Samosir_project/main/urls.py
+++ NEW_Samosir_project/Samosir_project/main/urls.py
@@ -5,5 +5,9 @@ app_name = 'main'
 urlpatterns = [
     path('', views.index, name='index'),
     path('shelters', views.shelters, name='all_shelters'),
-    path('shelters/<slug:shelter_slug>', views.shelter_view, name='shelter')
+    path('shelters/<slug:shelter_slug>', views.shelter_view, name='transport'),
+    path('transports', views.transports, name='all_transports'),
+    path('transports/<slug:transport_slug>', views.shelter_view, name='transport'),
+    path('funs', views.funs, name='all_funs'),
+    path('funs/<slug:fun_slug>', views.fun_view, name='fun')
 ]
