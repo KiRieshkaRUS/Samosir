df1fd7357d8789dc35f83bc6f8d9920939af8755
Kostritsa Kirill <kir300604@yandex.ru>
2024-11-15 18:26:08 +0300

    РАБОЧИЙ ПРОЕКТ

--- NEW_Samosir_project/Samosir_project/main/models.py
+++ NEW_Samosir_project/Samosir_project/main/models.py
@@ -33,3 +33,66 @@ class ShelterPhoto(models.Model):
         verbose_name_plural = 'Фото'
 
 
+class Transport(models.Model):
+    transport_name = models.CharField(max_length=250, verbose_name='Название транспорта', blank=True, null=True)
+    description = models.TextField(verbose_name='Описание', blank=True, null=True)
+    type = models.TextField(max_length=150, verbose_name='Тип', blank=True, null=True)
+    speed = models.TextField(max_length=250, verbose_name='Время доставки', blank=True, null=True)
+    price = models.TextField(max_length=150, verbose_name='Стоимость', blank=True, null=True)
+    rate = models.TextField(max_length=150, verbose_name='Рейтинг', blank=True, null=True)
+    transport_slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='URL')
+
+    def __str__(self):
+        return self.transport_name
+
+    def get_absolute_url(self):
+        return f'/transports/{self.transport_slug}'
+
+    class Meta:
+        verbose_name = 'Транспорт'
+        verbose_name_plural = 'Траспорт'
+
+
+class TransportPhoto(models.Model):
+    transport = models.ForeignKey(Transport, on_delete=models.CASCADE, verbose_name='Транспорт')
+    photo = models.ImageField(verbose_name='Фото', null=True, upload_to='media/transport_photo')
+
+    def __str__(self):
+        return self.transport.transport_name
+
+    class Meta:
+        verbose_name = 'Фото'
+        verbose_name_plural = 'Фото'
+
+
+class Fun(models.Model):
+    fun_name = models.CharField(max_length=250, verbose_name='Название', blank=True, null=True)
+    description = models.TextField(verbose_name='Описание', blank=True, null=True)
+    type = models.TextField(max_length=150, verbose_name='Тип', blank=True, null=True)
+    speed = models.TextField(max_length=250, verbose_name='Месторасположение', blank=True, null=True)
+    price = models.TextField(max_length=150, verbose_name='Стоимость', blank=True, null=True)
+    rate = models.TextField(max_length=150, verbose_name='Рейтинг', blank=True, null=True)
+    fun_slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='URL')
+
+    def __str__(self):
+        return self.fun_name
+
+    def get_absolute_url(self):
+        return f'/funs/{self.fun_slug}'
+
+    class Meta:
+        verbose_name = 'Развлечение'
+        verbose_name_plural = 'Развлечения'
+
+
+class FunPhoto(models.Model):
+    fun = models.ForeignKey(Fun, on_delete=models.CASCADE, verbose_name='Развлечение')
+    photo = models.ImageField(verbose_name='Фото', null=True, upload_to='media/fun_photo')
+
+    def __str__(self):
+        return self.fun.fun_name
+
+    class Meta:
+        verbose_name = 'Фото'
+        verbose_name_plural = 'Фото'
+
--- NEW_Samosir_project/Samosir_project/main/views.py
+++ NEW_Samosir_project/Samosir_project/main/views.py
@@ -20,3 +20,35 @@ def shelter_view(request, shelter_slug):
     context = {'shelter': shelter}
 
     return render(request, 'main/shelter.html', context)
+
+
+def transports(request):
+    all_transports = Transport.objects.order_by('transport_name')
+
+    context = {'all_transports': all_transports}
+
+    return render(request, 'main/transports.html', context)
+
+
+def transport_view(request, transport_slug):
+    transport = Transport.objects.get(transport_slug=transport_slug)
+
+    context = {'transport': transport}
+
+    return render(request, 'main/transport.html', context)
+
+
+def funs(request):
+    all_funs = Fun.objects.order_by('fun_name')
+
+    context = {'all_funs': all_funs}
+
+    return render(request, 'main/funs.html', context)
+
+
+def fun_view(request, fun_slug):
+    fun = Fun.objects.get(fun_slug=fun_slug)
+
+    context = {'fun': fun}
+
+    return render(request, 'main/fun.html', context)
